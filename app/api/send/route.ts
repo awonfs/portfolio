5bfd0c1a839970cae154e8f9cf5c80cbf444af5f
Eetu Rönkkö <105866472+awonfs@users.noreply.github.com>
2023-08-12 10:37:09 +0300

    Email now gets forwared to the right place

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -8,9 +8,9 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export async function POST() {
   try {
     const data = await resend.emails.send({
-      from: process.env.EMAIL!!,
+      from: process.env.EMAILFROM!!,
       to: [process.env.EMAIL!!],
-      subject: "Hello world",
+      subject: "Where is my cat?",
       react: EmailTemplate({ firstName: "Eetu" }) as React.ReactElement,
     });
 
